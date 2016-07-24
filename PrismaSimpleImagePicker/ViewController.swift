6fc67025c0ff6e4e89d127a73ad356ea99a638ca
Roylee-ML <roylee.stillway@gmail.com>
2016-07-24 14:45:54 +0800

    Initial Commit

---
+++ PrismaSimpleImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrismaSimpleImagePicker
+//
+//  Created by Roy lee on 16/7/24.
+//  Copyright © 2016年 Roy lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
